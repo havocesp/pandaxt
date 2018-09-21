dcf3bf1cc5f4b5a15d247e6374a2fa223cd5c142
havocesp <umpierrez@pm.me>
2018-09-21 05:52:11 +0200

    Added more info and fixed trailing comma error

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
-from setuptools import setup
-from pandaxt import __version__, __author__, __description__, __site__, __license__, __appname__,
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+from pandaxt import (__version__, __author__, __description__, __site__, __email__, __license__, __keywords__)
+
+exclude = ['.idea*', 'build*', '{}.egg-info*'.format(__package__), 'dist*', 'venv*', 'doc*', 'lab*']
 
 setup(
-    name=__appname__,
+    name=__package__,
     version=__version__,
-    packages=[''],
-    package_dir={'': __package__},
+    packages=find_packages(exclude=exclude),
     url=__site__,
     license=__license__,
+    packages_dir={'': __package__},
+    keywords=__keywords__,
     author=__author__,
-    author_email='',
-    description=__description__
+    author_email=__email__,
+    long_description=__description__,
+    description=__description__,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'pandas',
+        'tulipy',
+        'ccxt'
+    ]
 )
