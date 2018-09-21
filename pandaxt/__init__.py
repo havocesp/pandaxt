1fc57a20bc73cca38618f91b15617d9e54e7000f
Daniel J. Umpierrez <umpierrez@pm.me>
2018-09-21 05:50:47 +0200

    - Fixed trailing comma error  - Added __email__ and __keywords__ metadata.  - __appname__ renamed to __name__

--- pandaxt/__init__.py
+++ pandaxt/__init__.py
@@ -2,15 +2,17 @@
 """
 A Python 3 "ccxt" wrapper over "Pandas" lib.
 """
-__appname__ = 'PandaXT'
+__name__ = 'PandaXT'
 __package__ = 'pandaxt'
 __author__ = 'Daniel J. Umpierrez'
 __license__ = 'MIT'
 __version__ = '0.1.0'
 __description__ = 'A Python 3 "ccxt" wrapper over "Pandas" lib.'
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
-
+__email__ = 'umpierrez@pm.me'
+__keywords__ = ['altcoins', 'altcoin', 'exchange']
 
 from core import PandaXT
 
-__all__ = ['__description__', '__author__', '__license__', '__version__', '__package__', '__appname__', 'PandaXT']
+__all__ = ['__description__', '__author__', '__license__', '__version__', '__package__', '__name__', '__site__',
+           '__email__', '__keywords__', 'PandaXT']
