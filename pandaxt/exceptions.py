92ff509238e14de6ef6f720150c0359e2c3f3ffb
Daniel J. Umpierrez <umpierrez@pm.me>
2019-03-21 18:35:42 +0100

    New exceptions module.

---
+++ pandaxt/exceptions.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+"""exceptions module"""
+
+__all__ = ['TimeframeError', 'SymbolError', 'CurrencyError', 'SideError']
+
+
+class TimeframeError(ValueError):
+
+    def __init__(self, timeframe, exchange=None):
+        exchange = f' {str(exchange)}' if exchange else '.'
+        super().__init__(f'{str(timeframe)} is not a valid timeframe for exchange{exchange}')
+
+
+class SideError(ValueError):
+
+    def __init__(self, side):
+        super().__init__(f'Invalid side: {side} (accepted values: "buy", "sell")')
+
+
+class SymbolError(ValueError):
+
+    def __init__(self, symbol=None, exchange=None):
+        exchange = f' {exchange}' if exchange else str()
+        symbol = f' {str(symbol)}' if symbol else str()
+        super().__init__(f'{symbol} is not listed at exchange{exchange}')
+
+
+class CurrencyError(ValueError):
+
+    def __init__(self, currency=None, exchange=None):
+        exchange = f' {exchange}' if exchange else str()
+        currency = f' {str(currency)}' if currency else str()
+        super().__init__(f'{currency} is not listed at exchange{exchange}')
